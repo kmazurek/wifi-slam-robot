cb5a360b19aef57ffa0b03b33f4409ce56c9c287
Jakub Mazurek <zakaprov@gmail.com>
2017-04-06 01:34:19 +0200

    Created separate network and Sweep modules

---
+++ src/main.py
@@ -0,0 +1,27 @@
+import network.tcp_socket as tcp
+import network.wifi_scanner as scanner
+import sweep.sweep_lidar as sweep
+import sys
+import threading
+import time
+is_py2 = sys.version[0] == '2'
+if is_py2:
+    import Queue as queue
+else:
+    import queue as queue
+
+if __name__ == '__main__':
+    tcpSocketStop = threading.Event()
+    socketInputQueue = queue.Queue()
+    tcpSocketThread = tcp.TcpSocketThread(tcpSocketStop, socketInputQueue)
+    tcpSocketThread.start()
+
+    result = ''
+    try:
+        while True:
+            for cell in scanner.scan('wlan1'):
+                result += cell.ssid + " " + str(cell.signal) + "\n"
+            socketInputQueue.put(result)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        tcpSocketStop.set() # TODO Handle network exceptions?
