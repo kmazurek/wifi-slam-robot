0f3f84ed01ea5ce72bcdac328e68fe50c4ad61a7
Kuba Mazurek <zakaprov@gmail.com>
2017-11-04 19:34:23 +0100

    Reworked existing server code to use asyncio and websockets

--- wifi-slam-server/main.py
+++ wifi-slam-server/main.py
@@ -1,50 +1,29 @@
-import socket
-import sys
+import argparse
+import asyncio
+import websockets
 import json
-from breezyslam.components import Laser
 
-MAP_SIZE_PIXELS = 800
-MAP_SIZE_METERS = 32
+parser = argparse.ArgumentParser(description='Connect to the robot and start receiving data.')
+parser.add_argument('hostname')
+parser.add_argument('port')
+args = parser.parse_args()
+
+response_index: int = 0
 
 
 def print_json_response(response):
     global response_index
     try:
         print(json.loads(response))
-        print("Received response with index {}".format(response_index))
+        print(f"Received response with index {response_index}")
         response_index += 1
     except ValueError:
         print("Error decoding JSON response")
 
 
-class SweepLaser(Laser):
-    def __init__(self):
-        Laser.__init__(self, 355, 3, 355, 60000)
+async def client_loop():
+    async with websockets.connect(f"ws://{args.hostname}:{args.port}") as socket:
+        async for message in socket:
+            print_json_response(message)
 
-tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcp_socket.connect(("192.168.0.192", 8889))
-
-response_index = 0
-response_buffer = ""
-
-try:
-    while True:
-        response = tcp_socket.recv(8192)
-        print("Response length: {}".format(len(response)))
-
-        if not response:
-            continue
-
-        decoded_response = response.decode('utf-8')
-
-        for char in decoded_response:
-            if char == '\n':
-                print_json_response(response_buffer)
-                response_buffer = ""
-            else:
-                response_buffer += char
-
-
-except KeyboardInterrupt:
-    tcp_socket.close()
-    sys.exit(0)
+asyncio.get_event_loop().run_until_complete(client_loop())
