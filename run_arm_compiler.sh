a814093fd56e07eb9b5d3e8babea4999d1b6a959
Kuba Mazurek <zakaprov@gmail.com>
2017-11-12 17:20:26 +0100

    Updated dir names in Docker compiler script

--- run_arm_compiler.sh
+++ run_arm_compiler.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-docker run --rm -it -v $(pwd):${1:-/home/robot/wifi-mapper} -w ${1:-/home/robot/wifi-mapper} ev3dev/debian-jessie-cross
+docker run --rm -it -v $(pwd):${1:-/home/robot/wifi-slam-robot} -w ${1:-/home/robot/wifi-slam-robot} ev3dev/debian-jessie-cross
