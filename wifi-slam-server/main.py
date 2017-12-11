6fb54171b9ae278b0c60177a603c3403d4e267e8
Kuba Mazurek <zakaprov@gmail.com>
2017-12-11 13:33:45 +0100

    Demo: creating maps using BreezySLAM

--- wifi-slam-server/main.py
+++ wifi-slam-server/main.py
@@ -2,20 +2,68 @@ import argparse
 import asyncio
 import websockets
 import json
+from slam import *
+
+CM_TO_MM = 10
 
 parser = argparse.ArgumentParser(description='Connect to the robot and start receiving data.')
 parser.add_argument('hostname')
 parser.add_argument('port')
 args = parser.parse_args()
 
+slam = create_slam()
 
-def print_json_response(response):
+
+def pgm_save(filename, imgbytes, imgsize):
+    print('\nSaving image to file %s' % filename)
+
+    output = open(filename, 'wt')
+
+    output.write('P2\n%d %d 255\n' % imgsize)
+
+    wid, hgt = imgsize
+
+    for y in range(hgt):
+        for x in range(wid):
+            output.write('%d ' % imgbytes[y * wid + x])
+        output.write('\n')
+
+    output.close()
+
+
+def update_slam(scan):
+    # filtered = list(filter(lambda sample: sample[1] != 1, scan))
+    translated = list(map(lambda sample: sample if sample[1] != 1 else [sample[0], 0, sample[2]], scan))
+    mapped = list(map(lambda sample: sample[1] * CM_TO_MM, translated))
+
+    print(f'Received {len(mapped)} LIDAR samples . . .')
+    if len(mapped) == SCAN_SIZE:
+        print('Updating SLAM . . .')
+        slam.update(mapped)
+
+
+def adjust_scan(scan):
+    scan_length = len(scan)
+    scan_diff = SCAN_SIZE - scan_length
+    if scan_diff == 0 or abs(scan_diff) > 5:
+        return scan
+    else:
+        for i in range(1, abs(scan_diff)):
+            if scan_diff > 0:
+                scan.append(0)
+            else:
+                scan.pop()
+
+
+def process_packet(response):
     try:
         response = json.loads(response)
         wifi_scan = response['wifi_scan']
         sweep_scan = response['sweep_scan']
 
-        print(f'{wifi_scan}\n{sweep_scan}\n')
+        print(json.dumps(response))
+        update_slam(sweep_scan)
+
     except ValueError:
         print("Error decoding JSON response")
 
@@ -23,6 +71,11 @@ def print_json_response(response):
 async def client_loop():
     async with websockets.connect(f"ws://{args.hostname}:{args.port}") as socket:
         async for message in socket:
-            print_json_response(message)
+            process_packet(message)
 
-asyncio.get_event_loop().run_until_complete(client_loop())
+try:
+    asyncio.get_event_loop().run_until_complete(client_loop())
+except KeyboardInterrupt:
+    mapbytes = bytearray(MAP_SIZE_PIXELS * MAP_SIZE_PIXELS)
+    slam.getmap(mapbytes)
+    pgm_save('slam_map.pgm', mapbytes, (MAP_SIZE_PIXELS, MAP_SIZE_PIXELS))
---
+++ wifi-slam-server/slam/__init__.py
@@ -0,0 +1,15 @@
+from breezyslam.algorithms import RMHC_SLAM
+from breezyslam.components import Laser
+
+MAP_SIZE_PIXELS = 800
+MAP_SIZE_METERS = 16
+SCAN_SIZE = 355
+
+
+class SweepLaser(Laser):
+    def __init__(self):
+        Laser.__init__(self, 355, 3, 355, 60000)
+
+
+def create_slam():
+    return RMHC_SLAM(SweepLaser(), MAP_SIZE_PIXELS, MAP_SIZE_METERS)
