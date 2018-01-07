3102f6995ae173cecee2b61246b0938bd3b71634
Kuba Mazurek <zakaprov@gmail.com>
2018-01-07 21:34:44 +0100

    Added mock Sweep scanning thread

--- wifi-slam-robot/sweep/mock.py
+++ wifi-slam-robot/sweep/mock.py
@@ -1,6 +1,8 @@
+from model import SweepSample, SyncValue
+from random import randint
+from time import sleep
+from threading import Thread, Event
 from typing import AsyncGenerator, List
-from model import SweepSample
-import random
 
 degrees_count = 360
 
@@ -14,14 +16,14 @@ scan_values = {}
 
 
 def random_deviation():
-    return random.randint(-max_value_deviation, max_value_deviation)
+    return randint(-max_value_deviation, max_value_deviation)
 
 
 def __init_scan_values__():
     section_step = degrees_count / scan_sections
     for i in range(scan_sections):
-        base_distance = random.randint(0, max_distance)
-        base_signal = random.randint(0, max_signal_strength)
+        base_distance = randint(0, max_distance)
+        base_signal = randint(0, max_signal_strength)
 
         start = round(i * section_step)
         end = round(start + section_step)
@@ -50,3 +52,21 @@ async def mock_scan_generator() -> AsyncGenerator[List[SweepSample], None]:
 
     while True:
         yield __generate_scan__()
+
+
+class MockSweepThread(Thread):
+    def __init__(self, stop_event: Event, output_value: SyncValue[List[SweepSample]]):
+        Thread.__init__(self)
+        self.stop_event = stop_event
+        self.name = 'Mock Sweep thread'
+        self.output_value = output_value
+
+    def run(self):
+        print(f'Starting {self.name} . . .')
+        __init_scan_values__()
+
+        while not self.stop_event.is_set():
+            self.output_value.set(__generate_scan__())
+            sleep(1)
+
+        print('Stopping %s . . .' % self.name)
