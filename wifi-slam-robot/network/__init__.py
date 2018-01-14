62af639fdfc7808c343822bb687e9a0c08e63223
Kuba Mazurek <zakaprov@gmail.com>
2018-01-14 17:55:48 +0100

    Added synchronous method for obtaining wifi scans

--- wifi-slam-robot/network/__init__.py
+++ wifi-slam-robot/network/__init__.py
@@ -1,4 +1,7 @@
 import requests
+from model import WifiSample
+from typing import List
+from wifi import Cell
 
 
 def send_request(request: dict, address: str, port: int) -> dict:
@@ -7,3 +10,7 @@ def send_request(request: dict, address: str, port: int) -> dict:
         return response.json()
     else:
         return None
+
+
+def scan_wifi_interface(interface: str) -> List[WifiSample]:
+    return list(map(lambda cell: WifiSample(cell.ssid, cell.signal), Cell.all(interface)))
