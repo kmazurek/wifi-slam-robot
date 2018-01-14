9fcd49847507af37f0bf29a37a181c37911e0cbe
Kuba Mazurek <zakaprov@gmail.com>
2018-01-14 21:42:39 +0100

    Moved all SLAM-related code to a separate session class

--- wifi-slam-server/slam/__init__.py
+++ wifi-slam-server/slam/__init__.py
@@ -1,6 +1,7 @@
 from breezyslam.algorithms import RMHC_SLAM
 from breezyslam.sensors import Laser
 
+CM_TO_MM = 10
 MAP_SIZE_PIXELS = 800
 MAP_SIZE_METERS = 16
 SCAN_SIZE = 360
@@ -11,5 +12,56 @@ class SweepLaser(Laser):
         Laser.__init__(self, SCAN_SIZE, 2, SCAN_SIZE, 60000)
 
 
-def create_slam():
-    return RMHC_SLAM(SweepLaser(), MAP_SIZE_PIXELS, MAP_SIZE_METERS)
+class SLAMSession:
+    def __init__(self):
+        self.slam = RMHC_SLAM(SweepLaser(), MAP_SIZE_PIXELS, MAP_SIZE_METERS)
+
+    def update_slam(self, scan):
+        processed_scan = self.__process_scan__(scan)
+        if len(processed_scan) == SCAN_SIZE:
+            print('Updating SLAM . . .')
+            self.slam.update(processed_scan)
+
+    def save_map(self, file_path='slam_map.pgm'):
+        map_bytes = bytearray(MAP_SIZE_PIXELS * MAP_SIZE_PIXELS)
+        self.slam.getmap(map_bytes)
+        self.__pgm_save__(file_path, map_bytes, (MAP_SIZE_PIXELS, MAP_SIZE_PIXELS))
+
+    def __process_scan__(self, scan):
+        translated = list(map(lambda sample: sample if sample[1] != 1 else [sample[0], 0, sample[2]], scan))
+        approximated_angles = list(map(lambda sample: (round(sample[0] / 1000), sample[1], sample[2]), translated))
+        angle_dict = {}
+
+        for sample in approximated_angles:
+            if sample[0] >= 1:
+                stored = angle_dict.get(sample[0])
+                if stored is None or (stored is not None and sample[1] != 0):
+                    angle_dict[sample[0]] = sample
+
+        unique_angles = list(angle_dict.values())
+
+        if abs(len(unique_angles) - SCAN_SIZE) > 10:
+            return []
+
+        length_diff = len(unique_angles) - SCAN_SIZE
+        if length_diff != 0:
+            for i in range(abs(length_diff)):
+                unique_angles.append((0, 0, 0))
+
+        return list(map(lambda sample: sample[1] * CM_TO_MM, unique_angles))
+
+    def __pgm_save__(self, filename, img_bytes, img_size):
+        print('\nSaving image to file %s' % filename)
+
+        output = open(filename, 'wt')
+
+        output.write('P2\n%d %d 255\n' % img_size)
+
+        wid, hgt = img_size
+
+        for y in range(hgt):
+            for x in range(wid):
+                output.write('%d ' % img_bytes[y * wid + x])
+            output.write('\n')
+
+        output.close()
