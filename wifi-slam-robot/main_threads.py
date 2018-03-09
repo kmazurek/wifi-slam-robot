14d2429701564ebfa5e6089787a0af2e79e259cd
Kuba Mazurek <zakaprov@gmail.com>
2018-03-09 18:58:09 +0100

    WIP: tracking robot states using a state machine

--- wifi-slam-robot/main_threads.py
+++ wifi-slam-robot/main_threads.py
@@ -1,5 +1,6 @@
 import argparse
-from sync import SyncValue
+from model import RobotStates
+from sync import SyncValue, StateMachine
 from network import scan_wifi_interface
 from network import send_request
 from sweep import SweepThread
@@ -22,6 +23,7 @@ def is_scan_valid(scan: List) -> bool:
 
 if __name__ == '__main__':
     # TODO Add motor thread with a stop and pause flag? Or a state machine?
+    state_machine = StateMachine(RobotStates, RobotStates.INITIALISING)
 
     sweep_stop_event = Event()
     sweep_scan_holder = SyncValue()
@@ -30,12 +32,15 @@ if __name__ == '__main__':
 
     try:
         while True:
+            # TODO Have sweep thread set a flag/state to indicate new value, only process it then?
             sweep_scan = sweep_scan_holder.get()
             wifi_scan = scan_wifi_interface(args.wlan_interface)
             if is_scan_valid(sweep_scan) and is_scan_valid(wifi_scan):
                 sample = {'wifi_scan': wifi_scan, 'sweep_scan': sweep_scan}
                 # TODO Store scans locally regardless of network result, create a new document for each scanning session
+                # TODO Receive heading from server, store it in a sync value
                 response = send_request(sample, args.hostname, args.port)
-            sleep(2)
+                state_machine.set_state(RobotStates.MOVING)
+            sleep(1)
     except KeyboardInterrupt:
         sweep_stop_event.set()
--- wifi-slam-robot/model/__init__.py
+++ wifi-slam-robot/model/__init__.py
@@ -1,4 +1,12 @@
+from enum import Enum
 from typing import NamedTuple
+
+
+class RobotStates(Enum):
+    INITIALISING = 0
+    MOVING = 1
+    STOPPED = 2
+    FINISHED = 3
 
 
 class WifiSample(NamedTuple):
